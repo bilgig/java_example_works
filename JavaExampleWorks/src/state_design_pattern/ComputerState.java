90a040052172dd10b8ac0f1e626dfe3fc4589076
Bilgi Özdemir <bilgig@hotmail.com>
2023-08-22 12:24:00 +0300

    All Files

---
+++ JavaExampleWorks/src/state_design_pattern/ComputerState.java
@@ -0,0 +1,10 @@
+package state_design_pattern;
+//how many states of the computer these states are defined as interface.
+public interface ComputerState {
+       void onOpen();
+
+       void onClose();
+
+    }
+
+
